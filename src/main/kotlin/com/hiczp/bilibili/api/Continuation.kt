948da174125016e40ab64de242a4814892bfbea4
chocotan <loli@linux.com>
2021-08-21 09:47:10 +0800

    修改调用地址等

---
+++ src/main/kotlin/com/hiczp/bilibili/api/Continuation.kt
@@ -0,0 +1,7 @@
+package com.hiczp.bilibili.api
+
+abstract class Continuation<in T> : kotlin.coroutines.Continuation<T> {
+    abstract fun resume(value: T)
+    abstract fun resumeWithException(exception: Throwable)
+    override fun resumeWith(result: Result<T>) = result.fold(::resume, ::resumeWithException)
+}   
